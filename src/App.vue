94a6b4c856006ba58018a378f7f2c245a23a19e1
NeoGlanding <neoglanding@protonmail.com>
2022-01-13 11:14:24 +0700

    auto commit without message

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1>Hello to Vue</h1>
+  <Card name="Ihsan" school-class="12 TKJ 1" phone-number="08568881510" />
+</template>
+
+<script>
+import Card from './components/Card.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Card
+  }
+}
+</script>
---
+++ src/components/Card.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="card">
+        <b>{{ name }}</b>
+        <p>{{ schoolClass }}</p>
+        <p>{{ phoneNumber }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['name', 'schoolClass', 'phoneNumber'],
+    data() {
+        return {
+            
+        }
+    },
+    methods: {
+
+    }
+}
+</script>
+
+<style scoped>
+    .card {
+        width: 10rem;
+        border: 1px solid red;
+    }
+
+    .card p {
+        margin: 0
+    }
+</style>
