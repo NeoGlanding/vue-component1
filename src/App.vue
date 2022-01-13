41a166bf8f9898071c0ded6cda0c47ace2825de4
NeoGlanding <neoglanding@protonmail.com>
2022-01-13 11:28:28 +0700

    props comunication

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <h1>Hello to Vue</h1>
-  <Card name="Ihsan" school-class="12 TKJ 1" phone-number="08568881510" />
+  <Card is-man="1" name="Ihsan" school-class="12 TKJ 1" phone-number="08568881510" />
 </template>
 
 <script>
@@ -10,6 +10,11 @@ export default {
   name: 'App',
   components: {
     Card
+  },
+  methods: {
+    getData(data) {
+      console.log(data)
+    }
   }
 }
 </script>
--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,21 +1,30 @@
 <template>
-    <div class="card">
+    <div @click="changeIsMan()" class="card">
         <b>{{ name }}</b>
         <p>{{ schoolClass }}</p>
         <p>{{ phoneNumber }}</p>
+        <p>{{ propsIsMan ? 'You\'re a man' : 'You are still a man'}}</p>
     </div>
 </template>
 
 <script>
+function trueOrFalse(data) {
+    if (data == 1) return true;
+    return false;
+}
+
 export default {
-    props: ['name', 'schoolClass', 'phoneNumber'],
+    props: ['name', 'schoolClass', 'phoneNumber', 'getData', "isMan"],
     data() {
         return {
-            
+            compData: 'Hello',
+            propsIsMan: trueOrFalse(this.isMan) 
         }
     },
     methods: {
-
+        changeIsMan() {
+            console.log(this.propsIsMan) 
+        }
     }
 }
 </script>
